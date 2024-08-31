6fd22381a0edd470772c82e184563b9aadb57b20
Vikramkumar9 <2103a51526@sru.edu.in>
2024-08-31 16:29:34 +0530

    function

---
+++ function.py
@@ -0,0 +1,13 @@
+#printing the square of the given number
+def square(x):
+    return x*x
+
+# for i in range(8):
+#     print(f"The square of {i} is {square(i)}")
+
+
+
+#Taking the input from the users
+x  = int(input())
+for x in range(x):
+    print(f"The square of {x} is {square(x)}")
