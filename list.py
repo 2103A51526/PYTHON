1f4d1dfaef912563ed15b0160b8d35d70000291f
Vikramkumar9 <2103a51526@sru.edu.in>
2024-08-31 14:34:52 +0530

    created list and sorted the list

---
+++ list.py
@@ -0,0 +1,14 @@
+#creating list
+
+list = ["vikram", "pgli", "jaan","sona"]
+
+#adding one more element in list
+
+list.append("rani")
+
+#printing the list
+print(list)
+
+#sorting the list
+list.sort()
+print(list)
