e03d851b04339db626863ca18b28e95eca781fb4
Vikramkumar9 <2103a51526@sru.edu.in>
2024-09-01 08:35:35 +0530

    It all about lambda function

---
+++ lambda.py
@@ -0,0 +1,10 @@
+# Regular function to add two numbers
+def add(x, y):
+    return x + y
+
+# Lambda function to do the same
+add_lambda = lambda x, y: x + y
+
+# Using the lambda function
+result = add_lambda(5, 3)
+print(result)  # Output: 8
